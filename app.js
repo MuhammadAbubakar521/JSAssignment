86666351cb8797a1bf1a7ae04c2148a728a6e9d1
MuhammadAbubakar521 <abubakarumer521@gmail.com>
2020-06-15 23:31:37 +0500

    JS assignment

---
+++ app.js
@@ -0,0 +1,361 @@
+//Chapter 1
+//Task 1
+// alert("Welcome");
+
+// Task 2
+// alert("Error! Please enter a valid password.");
+//Task 3
+// alert("Welcome to JS Land... \nHappy Coding!");
+//Task 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs. ");
+//Task 5 Screenshot
+
+// Task 6
+// function alertFunc() {
+//     alert('Alert works');
+// }
+//Chapter # 2 Task # 1
+
+
+// var a = "Username";
+// document.write("a")
+
+// Task 2
+// var myname = "Muhammad Abubakar"
+// document.write("myname")
+
+// Task 3 
+// var message = "Hello world";
+// alert (message);
+// // Task 4
+// var a = "Jhone Doe";
+// alert (a);
+// var b = "15 years old";
+// alert  (b);
+// var c = "Certified Mobile Application Development"
+// alert (c);
+// // Task 5
+// var d =" PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert (d);
+// // Task 6
+// var x = "My email address is";
+// var email = " example@example.com";
+// var z = x+email;
+// alert (z);
+// // Task 7
+// var e = "I am trying to learn from the Book";
+// var book = " A smarter way to learn JavaScript";
+// var f = e+book;
+// alert (f);
+// Task 8
+// document.write("Yah! I can write HTML content through javaScript")
+// Task 9
+// var u = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert (u);
+
+
+// Chapter # 3 Task # 1
+// var age = "I am 15 years old";
+// alert (age);
+
+// Task # 2
+
+
+// Task 3
+//  var birthyear = "My birth year is 1990"
+//  document.write("My birth year is 1990 <br> Data type of my ddeclared variable is number ");
+
+
+// Task 4 
+// var Visitorname = "John Doe"
+// document.write(" <b> John Doe </b> ordered <b> 5 T-shirt</b>(s) on XYZ Clothing store.");
+
+
+// Chapter # 4  Task #  1
+
+
+// Task 2 5 legal variable
+// var variablename = "XYZ"
+// var ali1 = "XYZ"
+// var $ali = "XYZ"
+// var  a  = "XYZ"
+// var useralert = "XYZ"
+
+// 5 Illegal variable
+// var variable name = "XYZ"
+// var 1ali = "XYZ"
+// var ?ali = "XYZ"
+// var var  = "XYZ"
+// var Rose = "XYZ"
+
+// Task 3
+// document.write (" <b>Rules for naming JS variables</b> <br> <br> Variable names can only contain , numbers, $ and _. For example: $my_1stVariable <br> Variable must begin with a letter, $ or _.For example: $name,_name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords")
+
+//Chapter # 5 Task 1
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+
+//Chapter # 5 Task # 2
+
+// var a = 3;
+// var b = 5;
+// var c = 5 - 3;
+
+//Chapter # 5 Task # 2
+
+// var a = 3;
+// var b = 5;
+// var c = a * b;
+
+//Chapter # 5 Task # 2
+
+// var a = 3;
+// var b = 5;
+// var c = a / b;
+
+//Chapter # 5 Task # 2
+
+// var a = 3;
+// var b = 5;
+// var c = a % b;
+
+//Chapter # 5 Task # 3
+
+// var a = 5;
+// var b = ++a 
+// var c = 7;
+// var d = b+c;
+// var e = --d;
+// var f = 3;
+// var g = e%f;
+// document.write("Value after variable declaration is undefined </br> Initial value: 5 </br> value after increment is: 6 </br> Value after addition is: 13 </br> Value after decrement is: 12 </br> The remender is: 0 ")
+
+
+//Chapter # 5 Task # 4
+
+// var  ticket = 600;
+// var totalticket = 5;
+// var a =  ticket * totalticket;
+
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+
+//Chapter # 5 Task # 5
+// document.write("Table of 4 </br> 4x1=4 </br> 4x2=8 </br> 4x3=12 </br> 4x4=16 </br> 4x5=20 </br> 4x6=24 </br> 4x7=28 </br> 4x8=32 </br> 4x9=36 </br> 4x10=40")
+
+//Chapter # 5 Task # 7
+
+// var item1 = 650;
+// var item2 = 100;
+// var qitm1 = 3;
+// var qitm2 = 7;
+// var shipcharges = 100;
+// var totalitem1 = item1*qitm1;
+// var totalitem2 = item2*qitm2;
+// var total = totalitem1+totalitem2+shipcharges;
+//  document.write("<h1> Shopping Cart</h1> </br> </br> Price of item 1 is 650 </br> Quantity of item 1 is 3 </br> Price of item 2 is 100 </br> Quantity of item 2 is 7 </br> Shipping Charges 100 </br> </br> Total cost of your order is 2750 ")
+
+//Chapter # 5 Task # 8
+
+// var tmarks = 980;
+// var Marksobtained = 804;
+// var percentage = Marksobtained / tmarks * 100;
+// document.write("<h1> Marks Sheet </h1> </br> </br> Total marks: 980 </br> Marks obtained: 804 </br> Percentage: 82.0408163265306%");
+
+//Chapter # 5 Task # 9
+
+// var USdollar = 104.80;
+// var SaudiRiyal = 28;
+// var Totalusdollar = 10;
+// var Totalsaudiriyal = 25;
+// var dollarprice = USdollar*Totalusdollar;
+// var saudiprice = SaudiRiyal*Totalsaudiriyal;
+// var pakistancurrency = dollarprice+saudiprice;
+
+// document.write("<h1> Currency in PKR </h1> </br> </br> Total Currency in PKR: 1748")
+
+
+//Chapter # 5 Task # 10
+
+
+// var a = (2 + 5) * 10 / 2;
+
+//Chapter # 5 Task # 11
+
+//  var currentyear = 2020;
+//  var birthyear = 2000;
+//  var myage = currentyear - birthyear;
+
+// document.write("<h1> Age Calculator</h1> <br> <br> Current Year: 2016 <br> Birth Year: 1992 <br> Your Age is: 24");
+
+//Chapter # 5 Task # 13
+
+// var fav = "chocolatechip";
+// var age = 15;
+// var maxage = 65;
+// var amount = 3;
+// var total = (maxage - age) * amount;
+
+// document.write("<h1> The Lifetime Supply Calculator </h1> <br> <br> Favourite Snack: chocolate chip <br> Current age: 15 <br> Estimated Maximum Age: 65 <br> Amount of snacks per day: 3 <br> You will need 150 chocolate chip to last you until the ripe old age of 65")
+
+// Chapter # 6-8 Task # 1
+
+// document.write("Result: <br> The value of a is: 10 <br> ...................... <br> The value of ++a is: 11 <br> Now the value of a is: 11 <br> <br> The value of a++ is: 11 <br> Now the value of a is: 12 <br> <br> The value of --a is: 11 <br>Now the value of a is: 11 <br> <br> The value of a-- is: 11 <br> Now the value of a is: 10 ")
+
+// Chapter # 6-8 Task # 2
+
+//  var a = 2;
+//  var b = 1;
+//  var result = --a - --b + ++b + b--;
+//  var  output = 1  - 0   +  1  + 1    
+//  alert (result);
+// document.write(" a is 2 <br> b 1 ?? <br>result is 3")
+
+// Chapter # 6-8 Task # 6
+
+// var English = 100;
+// var Math = 100;
+// var Urdu = 100;
+
+// var totalmarks = English + Math + Urdu;
+
+
+// var sub1obtained = 54;
+// var sub2obtained = 54;
+// var sub3obtained = 48;
+
+// var sub1percentage = sub1obtained / English * 100;
+// var sub2percentage = sub2obtained / Math * 100;
+// var sub3percentage = sub3obtained / Urdu * 100;
+
+
+// var totalobtainedmarks = sub1obtained + sub2obtained + sub3obtained;
+
+// var totalpercentage = totalobtainedmarks / totalmarks * 100;
+
+// document.write("<b> Subject Total Marks Obtained Marks Percentage</b> </br> <pre>English    100        54           54%  <pre>Math       100        54           54% <pre>Urdu       100        48           48% <pre>    <b>            300         156        52%  </b> " );
+
+// Chapter # 9-11 Task # 1
+
+
+// var city = prompt ("Enter Your city");
+
+// if(city == "karachi"){
+//     alert ( "Welcome to city of lights" )
+// }
+//  else if (city == "sindh" ){
+//      alert ("Welcome to Sindh")
+//  }
+// else {
+//     alert("Welcome to Pakistan")
+// }
+
+// Chapter # 9-11 Task # 2
+
+// var gender = prompt ("Enter your gender");
+
+// if(gender == "male"){
+//     alert ("Good morning Sir")
+// }
+// if(gender == "female"){
+//     alert("Good Morning Ma’am")
+// }
+
+// Chapter # 9-11 Task # 4
+
+// var fuel = +prompt ("remaining fuel in car");
+// if (fuel == 0.25){
+//     alert("refill the fuel ")
+// }
+
+// else if (fuel  < 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+// else  {
+//     alert ("No need for refill fuel")
+
+// }
+
+// Chapter # 9-11 Task # 5
+
+// var a = 4;
+// if (++a ===5){
+//     alert("given condition for variable a is true")
+// } True
+
+// var b = 82;
+// if(b++ ===83) {
+//     alert("given condition for variable b is true")
+// } false
+
+// var c = 12;
+// if (c++ === 13){
+//     alerrt ("condition 1 is true")
+// }
+// if(c === 13) {
+//     alert ("condition 2 is true")
+// }
+
+// if(++C < 14) {
+//     alert("condition 3 is true")
+// }
+// if (c === 14) {
+//     alert ("condition 4 is true")
+// }
+
+// var materialCost = 20000;
+// var labourCost = 2000;
+// var totalCost = materialCost + labourCost;
+
+// if(totalCost===labourCost+materialCost){
+//     alert("The cost equal");
+// }
+
+// if(true){
+//     alert("True");
+// } 
+// if(false){
+//     alert("False");;
+// }
+
+// if ("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// Chapter # 9-11 Task # 6
+
+// var English = 100;
+// var Math = 100;
+// var Urdu = 100;
+
+// var totalmarks = English + Math + Urdu;
+
+
+// var sub1obtained = 54;
+// var sub2obtained = 54;
+// var sub3obtained = 48;
+
+// var sub1percentage = sub1obtained / English * 100;
+// var sub2percentage = sub2obtained / Math * 100;
+// var sub3percentage = sub3obtained / Urdu * 100;
+
+
+// var totalobtainedmarks = sub1obtained + sub2obtained + sub3obtained;
+
+// var totalpercentage = totalobtainedmarks / totalmarks * 100;
+
+// var percentage = +prompt ("Enter your percentage")
+
+// if (percentage  > 70){
+//    document.write(" <b>Marks Sheet </b> <br> Total marks : 300 <br> Marks obtained : 219 <br> Percentage : 73% <br> Grade : B <br> Remarks : You need to improve")
+// }
+// if (percentage  == 70){
+//     document.write(" <b>Marks Sheet </b> <br> Total marks : 300 <br> Marks obtained : 219 <br> Percentage : 80% <br> Grade : B <br> Remarks : You need to improve")
+//  }
+//  else if (percentage < 60 ){
+//     document.write(" <b>Marks Sheet </b> <br> Total marks : 300 <br> Marks obtained : 140 <br>  Grade : Fail <br> Remarks : Sorry")
+
+//  }
